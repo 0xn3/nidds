9478f98905151840df5e67a260d698a2f68daea0
Prasanthc41m <58906808+prasanthc41m@users.noreply.github.com>
2020-08-23 21:46:13 +0530

    Update nidds-reverseproxy.sh

--- nidds-reverseproxy.sh
+++ nidds-reverseproxy.sh
@@ -10,7 +10,7 @@ cd /tmp
 sed -i 's/#/ /g' file.txt
 sed -i '7d;8d' file.txt
 sed -i "s/example.com/$domain/g" file.txt
-sed -i 's/try_files $uri $uri\/ =404/proxy_pass http:\/\/0.0.0.0:1020/g' file.txt
+sed -i 's/try_files $uri $uri\/ =404/proxy_pass http:\/\/127.0.0.1:1020/g' file.txt
 #read -p 'Enter server port number: ' port
 #sed -i 's/try_files $uri $uri\/ =404/proxy_pass http:\/\/localhost:$port/g' file.txt
 apt install -y python3-certbot-nginx && certbot --nginx 
